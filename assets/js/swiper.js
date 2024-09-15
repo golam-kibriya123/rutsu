3782acf5763cff56d6e07c3ad200071af18dc8f0
golam-kibriya123 <golamkibriya622@gmail.com>
2024-09-15 04:35:29 +0000

    added

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -3,4 +3,29 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    slidesPerView: 3,
+    breakpoints: {
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 10
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 10
+        },
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        }
+    },
+
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
 });
